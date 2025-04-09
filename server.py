2362a1ca95f076c17302ac07059d39b195f8ed3a
chris-alexiuk <chris@alexiuk.ca>
2025-04-09 12:50:37 -0400

    Adding Dice Roller

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from mcp.server.fastmcp import FastMCP
 from tavily import TavilyClient
 import os
+from dice_roller import DiceRoller
 
 load_dotenv()
 
@@ -13,6 +14,12 @@ def web_search(query: str) -> str:
     """Search the web for information about the given query"""
     search_results = client.get_search_context(query=query)
     return search_results
+
+@mcp.tool()
+def roll_dice(notation: str, num_rolls: int = 1) -> str:
+    """Roll the dice with the given notation"""
+    roller = DiceRoller(notation, num_rolls)
+    return str(roller)  
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
